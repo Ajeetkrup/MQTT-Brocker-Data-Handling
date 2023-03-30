d8c6b47d36ac8d5cce6685a18ce88b9b2760897e
Ajeet Kumar Upadhyay <ajeetkrup401@gmail.com>
2023-03-30 18:01:29 +0530

    dockerized the app and db

--- api/config/postgres.js
+++ api/config/postgres.js
@@ -1,19 +1,19 @@
 const Pool = require('pg').Pool
 
-// const pool = new Pool({
-//   user: process.env.PG_USER,
-//   host: process.env.PG_HOST,
-//   database: process.env.PG_DB,
-//   password: process.env.PG_PASS,
-//   port: 5432,
-// });
-
 const pool = new Pool({
-  user: 'me',
-  host: 'localhost',
-  database: 'mbdh',
-  password: 'password',
+  user: process.env.PG_USER,
+  host: process.env.PG_HOST,
+  database: process.env.PG_DB,
+  password: process.env.PG_PASS,
   port: 5432,
 });
+
+// const pool = new Pool({
+//   user: 'me',
+//   host: 'localhost',
+//   database: 'mbdh',
+//   password: 'password',
+//   port: 5432,
+// });
 
 module.exports = pool;
